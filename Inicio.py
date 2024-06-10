656b2ab00e6e5144e8eea2e391ef72207599ae45
BautiButhet <jbbuthet@mail.austral.edu.ar>
2024-06-10 01:30:19 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -108,8 +108,8 @@ with tab1:
         }
         </style>
         """, unsafe_allow_html=True)
-    col1, col2,col3,col4,col5,col6,col7 = st.columns(7)
-    with col4:
+    col1, col2,col3,col4,col5,col6,col7,col8,col9 = st.columns(9)
+    with col5:
         if st.button("Comenzar"):
             st.switch_page("pages/1_Alta_Usuario.py")
 
